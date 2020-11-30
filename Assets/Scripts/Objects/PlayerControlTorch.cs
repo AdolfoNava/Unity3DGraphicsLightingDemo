3cbfbd58f7b4cd663412d9e52c427b4cfcfbf964
AdolfoNava <adolfonava1990@gmail.com>
2020-11-29 19:13:43 -0600

    Light Object Player Control
    First commit of the week
    Changes:
    - Made the Torch as a Prefab
    - Locked the cursor when the game is running
    - Made a new Script called PlayerControlLight that should change the light source relative to the position of the user within the enclosure
    - Left error code to try and move the light source manually but that didn't work when I locked the cursor in place
    Problems:
    - The new torch can't be assigned to the prefab projectile under the notation that the projectile isn't in the scene at the start

---
+++ Assets/Scripts/Objects/PlayerControlTorch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlTorch : MonoBehaviour
+{
+    //Vector3 mPosChange = Vector3.zero;
+    //Vector3 mBeforePos = Vector3.zero;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = target.position - transform.position;
+
+        // the second argument, upwards, defaults to Vector3.up
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        transform.rotation = rotation;
+        /*mPosChange = Input.mousePosition - mBeforePos;
+        if (Input.GetMouseButton(0))
+        {
+            if (Vector3.Dot(transform.up, Vector3.up) >= 0)
+            {
+                transform.Rotate(transform.up, Vector3.Dot(mPosChange, Camera.main.transform.right), Space.World);
+            }
+            else
+            {
+                transform.Rotate(transform.up, -Vector3.Dot(mPosChange, Camera.main.transform.right), Space.World);
+            }
+            transform.Rotate(Camera.main.transform.right, Vector3.Dot(mPosChange, Camera.main.transform.up), Space.World);
+        }
+        mBeforePos = Input.mousePosition;
+        */
+    }
+
+}
