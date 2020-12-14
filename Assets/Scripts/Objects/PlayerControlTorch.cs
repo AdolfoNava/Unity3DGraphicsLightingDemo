6b1f5be849a770befed4abc8dd5452357ac95858
AdolfoNava <adolfonava1990@gmail.com>
2020-12-14 07:56:09 -0600

    Torch Transformation adjusted
    Changes:
    - Adjusted the PlayerControlTorch script so that it function with my camera now and not the mouse
    - Added a ray and raycast to my Main Camera
    Problems:
    - The raycast doesn't seem to work and haven't found another apporach for the raycast implemenation

--- Assets/Scripts/Objects/PlayerControlTorch.cs
+++ Assets/Scripts/Objects/PlayerControlTorch.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerControlTorch : MonoBehaviour
 {
-    //Vector3 mPosChange = Vector3.zero;
-    //Vector3 mBeforePos = Vector3.zero;
+    Vector3 mPosChange = Vector3.zero;
+    Vector3 mBeforePos = Vector3.zero;
     public Transform target;
     // Start is called before the first frame update
     void Start()
@@ -16,26 +16,27 @@ public class PlayerControlTorch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 relativePos = target.position - transform.position;
+        
+            mPosChange = Camera.main.transform.rotation.eulerAngles - mBeforePos;
+            if (Input.GetMouseButton(0))
+            {
+                if (Vector3.Dot(transform.up, Vector3.up) >= 0)
+                {
+                    transform.Rotate(transform.up, Vector3.Dot(mPosChange, Camera.main.transform.right), Space.World);
+                }
+                else
+                {
+                    transform.Rotate(transform.up, -Vector3.Dot(mPosChange, Camera.main.transform.right), Space.World);
+                }
+                transform.Rotate(Camera.main.transform.right, Vector3.Dot(mPosChange, Camera.main.transform.up), Space.World);
+            }
+            mBeforePos = Camera.main.transform.rotation.eulerAngles;
+        
+        
+    }
+}
+        //Vector3 relativePos = target.position - transform.position;
 
         // the second argument, upwards, defaults to Vector3.up
-        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
-        transform.rotation = rotation;
-        /*mPosChange = Input.mousePosition - mBeforePos;
-        if (Input.GetMouseButton(0))
-        {
-            if (Vector3.Dot(transform.up, Vector3.up) >= 0)
-            {
-                transform.Rotate(transform.up, Vector3.Dot(mPosChange, Camera.main.transform.right), Space.World);
-            }
-            else
-            {
-                transform.Rotate(transform.up, -Vector3.Dot(mPosChange, Camera.main.transform.right), Space.World);
-            }
-            transform.Rotate(Camera.main.transform.right, Vector3.Dot(mPosChange, Camera.main.transform.up), Space.World);
-        }
-        mBeforePos = Input.mousePosition;
-        */
-    }
-
-}
+        //Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        //transform.rotation = rotation;
--- Assets/Scripts/User/CameraControl.cs
+++ Assets/Scripts/User/CameraControl.cs
@@ -15,22 +15,38 @@ public class CameraControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
+        Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
-        transform.Rotate(0f, x, 0f);
-        headRotation += y;
-        headRotation = Mathf.Clamp(headRotation, -headRotationLimit, headRotationLimit);
-        cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
+    { 
+        
+            float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
+            transform.Rotate(0f, x, 0f);
+            headRotation += y;
+            headRotation = Mathf.Clamp(headRotation, -headRotationLimit, headRotationLimit);
+            cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
+        
         if (Input.GetKeyDown(KeyCode.E))
         {
             Launch();
+        }
+        RayCast();
+    }
+
+    void RayCast()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = transform.forward;
+
+        Ray ray = new Ray(origin, direction);
+
+        if(Physics.Raycast(ray, out RaycastHit raycast))
+        {
+            raycast.collider.GetComponent<PlayerControlTorch>().target.GetComponent<Transform>().rotation = Camera.main.transform.rotation; 
         }
     }
     void Launch()
